73153a768da7d286107c1455fc15ceb6f69b5908
Hristo Shirov <hshirov@abv.bg>
2021-03-28 18:20:18 +0300

    Add project files.

---
+++ Rent-Management-System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rent_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
