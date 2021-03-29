2532a1bae321681bea590a982b611d423a745791
Веселин Инзов <veselin.inzov@gmail.com>
2021-03-29 21:34:05 +0300

    Adding Invoice and Tenants Controllers and Views

---
+++ Rent-Management-System/Controllers/InvoiceController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rent_Management_System.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
