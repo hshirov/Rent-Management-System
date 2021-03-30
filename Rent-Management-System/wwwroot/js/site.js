758c0613ba1bbb4f35e19e2b9e93eab3e017138d
Hristo Shirov <hshirov@abv.bg>
2021-03-30 16:45:39 +0300

    Added sidebar to layout

--- Rent-Management-System/wwwroot/js/site.js
+++ Rent-Management-System/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
