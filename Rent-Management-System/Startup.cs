3651ce8b5cb95e562f34d8696aa076f979808e42
Hristo Shirov <hshirov@abv.bg>
2021-04-08 10:31:50 +0300

    Ensure database is created

--- Rent-Management-System/Startup.cs
+++ Rent-Management-System/Startup.cs
@@ -34,6 +34,12 @@ namespace Rent_Management_System
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<RmsContext>();
+                context.Database.EnsureCreated();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
